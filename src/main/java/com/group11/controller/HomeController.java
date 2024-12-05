d7ed33516de4c18c45fe7afeec20af6296b33472
NguyenHoaiTan <tannh3108@gmail.com>
2024-12-05 08:53:39 +0700

    load trang home

---
+++ src/main/java/com/group11/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.group11.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    public String home() {
+        return "index-3";
+    }
+}
