16fc1f3e849cb514a8910f7a5e8d6df9dc490e04
pthngws <pthngws@gmail.com>
2024-12-05 19:28:45 +0700

    update chat & ìnfo

--- src/main/java/com/group11/service/impl/ChatServiceImpl.java
+++ src/main/java/com/group11/service/impl/ChatServiceImpl.java
@@ -1,4 +1,33 @@
 package com.group11.service.impl;
 
-public class ChatServiceImpl {
+import com.group11.entity.ChatEntity;
+import com.group11.repository.ChatRepository;
+import com.group11.service.IChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ChatServiceImpl implements IChatService {
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Override
+    public List<Map<String, Object>> findDistinctSendersByReceiverId() {
+        return chatRepository.findDistinctSendersByReceiverId();
+    }
+
+
+    @Override
+    public ChatEntity save(ChatEntity chatEntity) {
+        return chatRepository.save(chatEntity);
+    }
+
+    @Override
+    public List<ChatEntity> findAll() {
+        return chatRepository.findAll();
+    }
 }
