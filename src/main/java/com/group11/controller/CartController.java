35ce5a44cd96c5d8d1c8ee58110725c348e83273
NguyenHoaiTan <tannh3108@gmail.com>
2024-12-05 14:15:44 +0700

    thêm HomeController

---
+++ src/main/java/com/group11/controller/CartController.java
@@ -0,0 +1,13 @@
+package com.group11.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+    @RequestMapping("/cart")
+    public String load()
+    {
+        return "cart";
+    }
+}
