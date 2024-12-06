ca585cdeab488f568345e412aa4a30fb1dc3cbb2
dt256 <buiducthang13022004@gmail.com>
2024-12-06 17:10:22 +0700

    add admin order

---
+++ src/main/java/com/group11/dto/response/LineItemResponse.java
@@ -0,0 +1,12 @@
+package com.group11.dto.response;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class LineItemResponse {
+    private String productName;
+    private double price;
+    private int quantity;
+    private double subtotal; // price * quantity
+}
